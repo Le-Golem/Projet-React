f9f8f6b44d5b729bcd9866def77512eb6c4017e4
Doriane77 <dlolliadu77@hotmail.fr>
2023-01-16 11:11:11 +0100

    Router

--- projet/src/App.js
+++ projet/src/App.js
@@ -41,3 +41,24 @@ function App() {
 }
 
 export default App;
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Favory from "./Contents/Favory";
+import Home from "./Contents/Home";
+import Movie from "./Contents/Movie";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favory" element={<Favory />} />
+          <Route path="/movie:id" element={<Movie />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
