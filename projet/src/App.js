5ac8e411c6e21624ec34b74a544a38aa81fc0d1c
PaulinedeValance <pauline.de.valance@gmail.com>
2023-01-17 16:07:27 +0100

    changement

--- projet/src/App.js
+++ projet/src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import "./App.scss";
 import Nav from "./Components/Nav";
 import Favory from "./Contents/Favory";
